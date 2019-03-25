e5bf97ba40844c1b07743cdc32a98792057d0ebd
enismaxim1 <enismaxim1@gmail.com>
2019-03-25 14:43:36 -0400

    added getters and setters to the ingredient class

--- app/src/main/java/com/example/foodapp/Ingredient.java
+++ app/src/main/java/com/example/foodapp/Ingredient.java
@@ -6,8 +6,28 @@ public class Ingredient {
     private String name;
     private String amount;
 
+    public Ingredient (){
+
+    }
     public Ingredient(String title, String quantity){
         name = title;
         amount = quantity;
     }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getAmount(){
+        return amount;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAmount(String amount){
+        this.amount = amount;
+    }
+    
 }
--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.foodapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Write a message to the database
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myRef.setValue("Hello, World!");
+
     }
+
 }
