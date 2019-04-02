7b6ca6a2e4bf1c1ca73cbbb31a6689df817410fa
enismaxim1 <enismaxim1@gmail.com>
2019-04-02 10:49:39 -0400

    Revert "added navigation menu and settings menu and everything"
    This reverts commit 2fa8390

--- app/src/main/java/com/example/foodapp/feedActivity.java
+++ app/src/main/java/com/example/foodapp/feedActivity.java
@@ -3,9 +3,6 @@ package com.example.foodapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
 public class feedActivity extends AppCompatActivity {
 
@@ -14,25 +11,5 @@ public class feedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
         Intent intent = getIntent();
-    }
-
-    public boolean onCreateOptionsMenu(Menu menu){
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item){
-        switch(item.getItemId()){
-            case R.id.item1:
-                Intent intent = new Intent(this, NavigationActivity.class);
-                this.startActivity(intent);
-                return true;
-            case R.id.item2:
-                break;
-            case R.id.item3:
-                break;
-        }
-        return true;
     }
 }
