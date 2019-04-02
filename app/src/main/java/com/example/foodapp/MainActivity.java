2fa83909ffbe03b97efa848942bb0ff6318ff31e
enismaxim1 <enismaxim1@gmail.com>
2019-04-02 10:33:59 -0400

    added navigation menu and settings menu and everything

--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.foodapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,8 +26,10 @@ public class MainActivity extends AppCompatActivity {
         myRef.setValue("Hello, World!");
 
     }
+
+
     public void goToFeed(View v){
-        Intent intent = new Intent(this, feedActivity.class);
+        Intent intent = new Intent(this, NavigationActivity.class);
         startActivity(intent);
     }
 
