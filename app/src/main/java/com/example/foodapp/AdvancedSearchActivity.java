9968d24a341633b09b36bdca0448d693101b618f
enismaxim1 <enismaxim1@gmail.com>
2019-05-06 07:29:32 -0700

    Finished simple and advanced search

---
+++ app/src/main/java/com/example/foodapp/AdvancedSearchActivity.java
@@ -0,0 +1,132 @@
+package com.example.foodapp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AdvancedSearchActivity extends AppCompatActivity {
+
+    public static String TAG = "AdvancedSearchActivity";
+
+    String recipeName;
+    String recipeCulture;
+    ArrayList<String> ingredientNames = new ArrayList<>();
+    ArrayList<String> excludeIngredientNames = new ArrayList<>();
+    boolean searchOnly;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advanced);
+        getIntent();
+        String text = getIntent().getStringExtra("input");
+        ingredientNames.clear();
+        excludeIngredientNames.clear();
+        EditText recipeSearch = findViewById(R.id.recipe_name);
+        recipeSearch.setText(text);
+
+        TextView ingredientsList = findViewById(R.id.ingredients_list);
+        ingredientsList.setText("Ingredients List:");
+
+        TextView excludeIngredientsList = findViewById(R.id.ingredients_list2);
+        excludeIngredientsList.setText("Ingredients List:");
+    }
+
+    public void addIngredient(View v) {
+        @SuppressLint("WrongViewCast") EditText add_ingredient_name = findViewById(R.id.ingredient1_name);
+        String ingName = add_ingredient_name.getText().toString();
+
+        if (TextUtils.isEmpty(ingName)) {
+            add_ingredient_name.setError("Required Input");
+        }
+
+        if (!TextUtils.isEmpty(ingName)) {
+
+            Button radio1 = findViewById(R.id.include_some);
+            radio1.setVisibility(View.VISIBLE);
+            Button radio2 = findViewById(R.id.include_only);
+            radio2.setVisibility(View.VISIBLE);
+
+            add_ingredient_name.setText("");
+            ingredientNames.add(ingName);
+            String str[] = new String[ingredientNames.size()];
+
+            // ArrayList to Array Conversion
+            for (int i = 0; i < ingredientNames.size(); i++) {
+
+                // Assign each value to String array
+                str[i] = ingredientNames.get(i);
+            }
+
+            @SuppressLint("WrongViewCast") TextView ingredientsList = findViewById(R.id.ingredients_list);
+            ingredientsList.setText("Ingredients List: " + Arrays.toString(str));
+        }
+    }
+
+    public void excludeIngredient(View v) {
+        @SuppressLint("WrongViewCast") EditText add_ingredient_name = findViewById(R.id.ingredient2_name);
+        String ingName = add_ingredient_name.getText().toString();
+
+        if (TextUtils.isEmpty(ingName)) {
+            add_ingredient_name.setError("Required Input");
+        }
+
+        if (!TextUtils.isEmpty(ingName)) {
+
+//            Button radio1 = findViewById(R.id.include_some);
+//            radio1.setVisibility(View.VISIBLE);
+//            Button radio2 = findViewById(R.id.include_only);
+//            radio2.setVisibility(View.VISIBLE);
+
+            add_ingredient_name.setText("");
+            excludeIngredientNames.add(ingName);
+            String str[] = new String[excludeIngredientNames.size()];
+
+            // ArrayList to Array Conversion
+            for (int i = 0; i < excludeIngredientNames.size(); i++) {
+
+                // Assign each value to String array
+                str[i] = excludeIngredientNames.get(i);
+            }
+
+            @SuppressLint("WrongViewCast") TextView ingredientsList2 = findViewById(R.id.ingredients_list2);
+            ingredientsList2.setText("Ingredients List: " + Arrays.toString(str));
+        }
+    }
+
+    public void initiateSearch(View v){
+        EditText name = findViewById(R.id.recipe_name);
+        recipeName = name.getText().toString();
+        EditText culture = findViewById(R.id.culture_name);
+        recipeCulture = culture.getText().toString();
+        RadioButton button = findViewById(R.id.include_some);
+        if (button.isChecked()){
+            searchOnly = false;
+        }  else {
+            searchOnly = true;
+        }
+        Log.d(TAG, button.isChecked() +"");
+        Log.d(TAG, searchOnly +"");
+        Intent intent = new Intent(this, SearchActivity.class);
+        intent.putExtra("recipeName", recipeName);
+        intent.putExtra("recipeCulture", recipeCulture);
+        intent.putExtra("ingredientNames", ingredientNames);
+        intent.putExtra("excludeIngredientNames", excludeIngredientNames);
+        intent.putExtra("searchOnly", searchOnly);
+        startActivity(intent);
+    }
+
+}
+
--- app/src/main/java/com/example/foodapp/SearchActivity.java
+++ app/src/main/java/com/example/foodapp/SearchActivity.java
@@ -18,51 +18,84 @@ import java.util.List;
 public class SearchActivity extends AppCompatActivity {
 
     public static String TAG = "SearchActivity";
+    String recipeName = null;
+    String recipeCulture = null;
+    ArrayList<String> ingredientNames = null;
+    ArrayList<String> excludeIngredientNames = null;
+    boolean searchOnly;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        getIntent();
-        MainActivity.readRecipe();
 
-//        ArrayList<String> search = new ArrayList<>();
-//        search.add("lemon");
-//
-//        RecyclerView recyclerView;
-//        ShowRecipeAdapter adapter;
-//        List<Recipe> recipeList;
-//        recipeList = findWithIngredients(search);
-//        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-//        recyclerView.setHasFixedSize(true);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-//        adapter = new ShowRecipeAdapter(this, recipeList);
-//        recyclerView.setAdapter(adapter);
+        getIntent();
+        recipeName = getIntent().getStringExtra("recipeName");
+        recipeCulture = getIntent().getStringExtra("recipeCulture");
+        ingredientNames = getIntent().getStringArrayListExtra("ingredientNames");
+        excludeIngredientNames = getIntent().getStringArrayListExtra("excludeIngredientNames");
+        searchOnly = getIntent().getBooleanExtra("searchOnly", false);
+
+
+        MainActivity.readRecipe();
+        ArrayList<Recipe> currentFilter = (ArrayList<Recipe>) MainActivity.recipes.clone();
+        if (recipeName != null && !(recipeName.isEmpty())) {
+            currentFilter = searchByName(recipeName, currentFilter);
+            Log.d(TAG, "step 1 " + currentFilter.size());
+        }
+        if (recipeCulture!= null && (!recipeCulture.isEmpty())) {
+            currentFilter = searchByCulture(recipeCulture, currentFilter);
+            Log.d(TAG, "step 2 " + currentFilter.size());
+        }
+        if (ingredientNames!= null && !ingredientNames.isEmpty() && !searchOnly) {
+            currentFilter = findByIngredient(ingredientNames, currentFilter);
+            Log.d(TAG, "step 3 " + currentFilter.size());
+        }
+        if (ingredientNames!= null && !ingredientNames.isEmpty() && searchOnly) {
+            currentFilter = findWithIngredients(ingredientNames, currentFilter);
+            Log.d(TAG, "step 3, findWithIngredients " + currentFilter.size());
+        }
+        if (excludeIngredientNames!= null && (!excludeIngredientNames.isEmpty())) {
+            currentFilter = excludeIngredients(excludeIngredientNames, MainActivity.recipes);
+            Log.d(TAG, "step 4 " + currentFilter.size());
+        }
+
+            RecyclerView recyclerView;
+            ShowRecipeAdapter adapter;
+            List<Recipe> recipeList;
+            recipeList = currentFilter;
+            recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            adapter = new ShowRecipeAdapter(this, recipeList);
+            recyclerView.setAdapter(adapter);
+
     }
 
     public void initiateSearch(View v) {
         EditText searchEditText = findViewById(R.id.inputSearch);
         String text = searchEditText.getText().toString();
 
-
         RecyclerView recyclerView;
         ShowRecipeAdapter adapter;
         List<Recipe> recipeList;
-        recipeList = searchByName(text);
+        recipeList = searchByName(text, MainActivity.recipes);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new ShowRecipeAdapter(this, recipeList);
         recyclerView.setAdapter(adapter);
+
+
     }
 
     /**
      * @param s a string for the name search
      * @return an arraylist of all recipes whose names are the same as s
      */
-    public ArrayList<Recipe> searchByName(String s) {
+    public ArrayList<Recipe> searchByName(String s, ArrayList<Recipe> base) {
         ArrayList<Recipe> ret = new ArrayList<>();
-        for (Recipe r : MainActivity.recipes) {
+        for (Recipe r : base) {
             if (r.getName().toLowerCase().contains(s.toLowerCase())) {
                 ret.add(r);
             }
@@ -74,10 +107,10 @@ public class SearchActivity extends AppCompatActivity {
      * @param s a string
      * @return
      */
-    public ArrayList<Recipe> searchByCulture(String s) {
+    public ArrayList<Recipe> searchByCulture(String s, ArrayList<Recipe> base) {
         ArrayList<Recipe> ret = new ArrayList<>();
-        for (Recipe r : MainActivity.recipes) {
-            if (r.getCulture().equals(s.toLowerCase())) {
+        for (Recipe r : base) {
+            if (r.getCulture().toLowerCase().contains(s.toLowerCase())) {
                 ret.add(r);
             }
         }
@@ -88,11 +121,11 @@ public class SearchActivity extends AppCompatActivity {
      * @param ingredients an arraylist of ingredient names
      * @return an arraylist of recipes that do not contain any of the ingredients in the arraylist
      */
-    public ArrayList<Recipe> excludeIngredients(ArrayList<String> ingredients) {
-        ArrayList<Recipe> temp = (ArrayList<Recipe>) MainActivity.recipes.clone();
-        for (Recipe r : MainActivity.recipes) {
+    public ArrayList<Recipe> excludeIngredients(ArrayList<String> ingredients, ArrayList<Recipe> base) {
+        ArrayList<Recipe> temp = (ArrayList<Recipe>) base.clone();
+        for (Recipe r : base) {
             for (Ingredient i : r.getIngredients()) {
-                if (ingredients.contains(i.getName().toLowerCase())) {
+                if (arrayToLowercase(ingredients).contains(i.getName().toLowerCase())) {
                     temp.remove(r);
                 }
             }
@@ -105,11 +138,11 @@ public class SearchActivity extends AppCompatActivity {
      * @param ingredients an arraylist of ingredient names
      * @return an arraylist of recipes that contain at least one ingredient
      */
-    public ArrayList<Recipe> findByIngredient(ArrayList<String> ingredients) {
+    public ArrayList<Recipe> findByIngredient(ArrayList<String> ingredients, ArrayList<Recipe> base) {
         ArrayList<Recipe> temp = new ArrayList<>();
-        for (Recipe r : MainActivity.recipes) {
+        for (Recipe r : base) {
             for (Ingredient i : r.getIngredients()) {
-                if (ingredients.contains(i.getName().toLowerCase())) {
+                if (arrayToLowercase(ingredients).contains(i.getName().toLowerCase()) && !temp.contains(r)) {
                     temp.add(r);
                 }
             }
@@ -121,11 +154,11 @@ public class SearchActivity extends AppCompatActivity {
      * @param ingredients an arraylist of ingredient names. Will always be lowercase
      * @return an arraylist of recipes that contain only ingredients in the arraylist
      */
-    public ArrayList<Recipe> findWithIngredients(ArrayList<String> ingredients) {
-        ArrayList<Recipe> temp = (ArrayList<Recipe>) MainActivity.recipes.clone();
-        for (Recipe r : MainActivity.recipes) {
+    public ArrayList<Recipe> findWithIngredients(ArrayList<String> ingredients, ArrayList<Recipe> base) {
+        ArrayList<Recipe> temp = (ArrayList<Recipe>) base.clone();
+        for (Recipe r : base) {
             for (Ingredient i : r.getIngredients()) {
-                if (!ingredients.contains(i.getName().toLowerCase())) {
+                if (!arrayToLowercase(ingredients).contains(i.getName().toLowerCase())) {
                     temp.remove(r);
                 }
             }
@@ -133,19 +166,19 @@ public class SearchActivity extends AppCompatActivity {
         return temp;
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.navigation, menu);
-        return true;
+    public void initiateAdvancedSearch(View v){
+        Intent intent = new Intent(this, AdvancedSearchActivity.class);
+        EditText searchEditText = findViewById(R.id.inputSearch);
+        String text = searchEditText.getText().toString();
+        intent.putExtra("input", text);
+        startActivity(intent);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.item1:
-                break;
+    public ArrayList<String> arrayToLowercase(ArrayList<String> arr){
+        ArrayList<String> ret = new ArrayList<String>();
+        for (int i=0; i<arr.size(); i++){
+            ret.add(arr.get(i).toLowerCase());
         }
-        return true;
+        return ret;
     }
 }
